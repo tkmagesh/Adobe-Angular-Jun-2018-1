0cb91ca9396ee768cfd5bb87c19540088cdb5ff7
Magesh <tkmagesh77@gmail.com>
2018-06-19 11:44:24 +0530

    Introducing Calculator Model

--- first-app/src/app/calculator/calculatorOne.component.ts
+++ first-app/src/app/calculator/calculatorOne.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CalculatorModel } from './calculatorModel';
 
 @Component({
 	selector : 'app-calculator-1',
@@ -8,17 +9,19 @@ export class CalculatorOneComponent{
 	n1 = 0;
 	n2 = 0;
 	result = 0 ;
-	
+
+	model : CalculatorModel = new CalculatorModel();
+
 	onAddClick(){
-		this.result = this.n1 + this.n2;
+		this.result = this.model.add(this.n1, this.n2);
 	}
 	onSubtractClick(){
-		this.result = this.n1 - this.n2;
+		this.result = this.model.subtract(this.n1, this.n2);
 	}
 	onMultiplyClick(){
-		this.result = this.n1 * this.n2;
+		this.result = this.model.multiply(this.n1, this.n2);
 	}
 	onDivideClick(){
-		this.result = this.n1 / this.n2;
+		this.result = this.model.divide(this.n1, this.n2);
 	}
 }
